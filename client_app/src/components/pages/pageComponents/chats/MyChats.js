c9b2333ad9f221ca46aa2628c78ade7ccf4dba3a
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2022-01-26 14:40:01 +0530

    Managing Search Code
    Signed-off-by: Kalyan Jyoti Borah <kalyanborah456@gmail.com>

--- client_app/src/components/pages/pageComponents/chats/MyChats.js
+++ client_app/src/components/pages/pageComponents/chats/MyChats.js
@@ -1,80 +1,95 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import Box from "@mui/system/Box";
-import SearchIcon from '@mui/icons-material/Search';
 import {ChatState} from "../../../context/ChatProvider";
-import {chatInstance} from "../../../../config/axios";
-import {alpha, styled} from "@mui/material/styles";
-import {Container, InputBase} from "@mui/material";
+import {authInstance} from "../../../../config/axios";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import InputBase from '@mui/material/InputBase';
+import {alpha, styled} from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import {Container} from "@mui/material";
+import Modal from "@mui/material/Modal";
+import ModalHeader from "react-bootstrap/ModalHeader";
 
 
+const Search = styled( 'div' )( ({theme}) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha( theme.palette.common.white, 0.15 ),
+	'&:hover': {
+		backgroundColor: alpha( theme.palette.common.white, 0.25 ),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up( 'sm' )]: {
+		marginLeft: theme.spacing( 1 ),
+		width: 'auto',
+	},
+}) );
+
+const SearchIconWrapper = styled( 'div' )( ({theme}) => ({
+	padding: theme.spacing( 0, 2 ),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}) );
+
+const StyledInputBase = styled( InputBase )( ({theme}) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing( 1, 1, 1, 0 ),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing( 4 )})`,
+		transition: theme.transitions.create( 'width' ),
+		width: '100%',
+		[theme.breakpoints.up( 'sm' )]: {
+			width: '12ch',
+		},
+	},
+}) );
+
+
+const Alert = React.forwardRef( function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+} )
 export default function MyChats() {
+	const [ search, setSearch ] = useState( " " )
+	const [ error, setError ] = useState( null );
+	const [ openS, setOpenS ] = React.useState( false );
+	const [ openM, setOpenM ] = useState( true )
+	const [ searchResults, setSearchResults ] = useState( [] );
 	const [ loggedUser, setLoggedUser ] = useState();
 	const {user, chats, setChats} = ChatState()
 
-	const Search = styled( 'div' )( ({theme}) => ({
-		position: 'relative',
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: alpha( theme.palette.common.white, 0.15 ),
-		'&:hover': {
-			backgroundColor: alpha( theme.palette.common.white, 0.25 ),
-		},
-		marginRight: theme.spacing( 2 ),
-		marginLeft: 0,
-		width: '100%',
-		[theme.breakpoints.up( 'sm' )]: {
-			marginLeft: theme.spacing( 3 ),
-			width: 'auto',
-		},
-	}) );
+	const handleCloseE = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setOpenS( false );
+		setOpenM( false )
+	};
 
-	const SearchIconWrapper = styled( 'div' )( ({theme}) => ({
-		padding: theme.spacing( 0, 2 ),
-		height: '100%',
-		position: 'absolute',
-		pointerEvents: 'none',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}) );
+	console.log( search )
 
-	const StyledInputBase = styled( InputBase )( ({theme}) => ({
-		color: 'inherit',
-		'& .MuiInputBase-input': {
-			padding: theme.spacing( 1, 1, 1, 0 ),
-			// vertical padding + font size from searchIcon
-			paddingLeft: `calc(1em + ${theme.spacing( 4 )})`,
-			transition: theme.transitions.create( 'width' ),
-			width: '100%',
-			[theme.breakpoints.up( 'md' )]: {
-				width: '20ch',
-			},
-		},
-	}) );
-
-	const accessChats = async (userId) => {
+	const handleSearch = async () => {
 		try {
-			const data = await chatInstance.post( "/",
-				{userId},
+			const {response} = await authInstance.get( `?search=${search}`,
 				{
 					headers:
 						{
-							'Content-Type': "application/json",
-							'Authorization': `Bearer${user.token}`,
+							Authorization: `Bearer ${user.token}`,
 						}
 				} )
-			console.log( data )
-			setChats( data )
+			console.log( response.data )
+			setSearchResults( response.data )
 		} catch (e) {
-			console.log( e )
+			setError( e.response.data.message )
 		}
-
 	};
-	useEffect( () => {
-		setLoggedUser( JSON.parse( localStorage.getItem( "userInfo" ) ) )
-		accessChats().then( r => {
-			console.log( r )
-		} )
-	}, [] )
+
 
 	return (
 		<>
@@ -91,13 +106,26 @@ export default function MyChats() {
 							<SearchIcon/>
 						</SearchIconWrapper>
 						<StyledInputBase
+							value={search}
+							onChange={(e) => {
+								setSearch( e.target.value )
+							}}
 							placeholder="Search…"
 							inputProps={{'aria-label': 'search'}}
 						/>
 					</Search>
+
+
 				</Box>
 				<Container maxWidth={"sm"}>
-
+					{error ? <Snackbar open={openS} autoHideDuration={3000} onClose={handleCloseE}>
+							<Alert onClose={handleCloseE} severity="error" sx={{width: '100%'}}>
+								{error}
+							</Alert>
+						</Snackbar>
+						: <Modal open={openM}>
+							<ModalHeader>Search</ModalHeader>
+						</Modal>}
 
 				</Container>
 			</Box>
